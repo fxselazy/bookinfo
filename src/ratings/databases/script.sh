d5d87218c78dd2c8d5e545c3830469ca4871a1a1
nirafxse <nirawat.fuse@mail.kmutt.ac.th>
2021-04-13 03:02:20 +0700

    [Update]ratings

--- src/ratings/databases/script.sh
+++ src/ratings/databases/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -e
-mongoimport --host localhost --db ratings \
-  --collection ratings --drop --file /docker-entrypoint-initdb.d/ratings_data.json
+mongoimport --host localhost --username $MONDODB_USERNAME --password $MONDODB_PASSWORD \
+ --db $MONDODB_DATABASE  --collection ratings --drop --file /docker-entrypoint-initdb.d/ratings_data.json
